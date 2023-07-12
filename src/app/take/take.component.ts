c5afad76286d39aa20358d881981ae93066d96d0
snixie <fsd.rabgay@gmail.com>
2023-07-12 17:24:39 +0600

    takeWhile

--- src/app/take/take.component.ts
+++ src/app/take/take.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { debounceTime, take } from 'rxjs';
+import { debounceTime, take, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-take',
@@ -10,6 +10,9 @@ import { debounceTime, take } from 'rxjs';
 export class TakeComponent implements OnInit {
   /**
    * Take (tells how many values to take)
+   * UPDATE
+   * takWhile (take the value and check the condition or not, if condition
+   * is met it will stop)
    */
 
   searchForm!: FormGroup;
@@ -24,7 +27,9 @@ export class TakeComponent implements OnInit {
     this.searchForm
       .get('name')
       ?.valueChanges.pipe(
-        take(2), // take 2 values
+        // take(2), // take 2 values
+        takeWhile((v) => this.checkCondition(v)),
+        // if true: continue else: break
         debounceTime(3000) // time lag before it emits next value
       )
       .subscribe((d) => {
@@ -33,4 +38,8 @@ export class TakeComponent implements OnInit {
   }
 
   readValue() {}
+
+  checkCondition(value: any) {
+    return value.length < 5 ? true : false;
+  }
 }
